49040d09a9f89425e5277428e38a143826999ab0
Brian Kung <brian@callmekung.com>
2014-05-07 21:53:53 -0500

    Moves jquery to be included in proper order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,12 +9,12 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-// Loads all Bootstrap javascripts
-//= require bootstrap
 // 
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//
+// Loads all Bootstrap javascripts
+//= require bootstrap
 
