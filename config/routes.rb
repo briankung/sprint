4a927e8b2a7ec5a755c4acb3f5f22c51b50fc59e
Andrew Johnson <andrew@trocharion.net>
2014-06-27 12:34:11 -0500

    Allow admins to change password, email, or delete their account
    Based on instructions at
    https://github.com/plataformatec/devise/wiki/How-To:-Allow-users-to-edit-their-password
    
    Devise is not set up to use the registrable module, so people cannot just
    sign up online. A few extra changes in this commit are needed to let
    people who have accounts change them.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Sprint::Application.routes.draw do
   devise_for :admins, skip: :registrations
+  as :admin do
+    get 'admins/edit' => 'devise/registrations#edit', :as => 'edit_admin_registration'
+    patch 'admins/:id' => 'devise/registrations#update', :as => 'admin_registration'
+  end
   resources :teams, :submissions, :rankings, :problems
   root to: 'pages#fivek'
   get '/rules', to: 'pages#rules'
