ba3d34d3c50a24bcf7daad4e3a3a8591a799a5af
Andrew Johnson <andrew@trocharion.net>
2014-07-09 17:10:55 -0500

    Show teams that have solved a problem against each problem

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,5 +1,15 @@
 class ProblemsController < ApplicationController
   def index
+    relation = ActiveRecord::Base.connection.execute "SELECT number, name, event_id, event, team FROM problems LEFT JOIN (SELECT events.id AS event_id, events.name AS event, teams.name AS team, submissions.problem AS problem FROM events LEFT JOIN teams ON events.id = teams.event_id LEFT JOIN submissions ON teams.id = submissions.team_id) ON problems.number = problem;"
+    @solved = {}
+    relation.each do |tuple|
+      if @solved[tuple['number']].nil?
+        @solved[tuple['number']] = {name: tuple['name'], solvers: []}
+      end
+      if tuple['event']
+        @solved[tuple['number']][:solvers] << tuple
+      end
+    end
   end
 
   def show
