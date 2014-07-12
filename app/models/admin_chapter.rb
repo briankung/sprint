f6c3130261bd4e8701e9496bc7f40df2d165e594
Andrew Johnson <andrew@trocharion.net>
2014-07-12 18:14:13 -0500

    Fix bug in AdminChapter model (symbol instead of variable identifier)

--- app/models/admin_chapter.rb
+++ app/models/admin_chapter.rb
@@ -3,7 +3,7 @@ class AdminChapter < ActiveRecord::Base
   belongs_to :chapter
 
   def self.authorized?(admin_id, chapter_id)
-    if AdminChapter.where(admin_id: admin_id, chapter_id: :chapter_id).empty?
+    if AdminChapter.where(admin_id: admin_id, chapter_id: chapter_id).empty?
       return false # Not associated => not authorized
     else
       return true # Associated => authorized
