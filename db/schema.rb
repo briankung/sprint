9fbfa5ae249b769ae455276bc89b2ae31115c3ff
Brian Kung <brian@callmekung.com>
2014-08-20 18:36:27 -0500

    Adds Google Analytics tracking ID

--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,14 @@ ActiveRecord::Schema.define(version: 20140712205548) do
     t.integer  "chapter_id"
   end
 
+  create_table "posts", force: true do |t|
+    t.string   "author"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "problems", id: false, force: true do |t|
     t.integer "number"
     t.text    "name"
