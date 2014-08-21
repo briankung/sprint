88d6f0e1a4ea266d81638fdd24603628a0ab3e98
Brian Kung <brian@callmekung.com>
2014-08-21 17:40:01 -0500

    Moves Admin stuff to navbar, wider layout, more nav links

--- config/routes.rb
+++ config/routes.rb
@@ -7,27 +7,19 @@ Sprint::Application.routes.draw do
   get 'problems' => 'problems#index', as: 'problems'
   get 'problem/:number' => 'problems#show', as: 'problem'
 
-  get 'chapters' => 'chapters#index', as: 'chapters'
-  get 'chapter/:id' => 'chapters#show', as: 'chapter'
+  get 'chapters' => 'chapters#index'
+  resources 'chapters', except: [:index], path: 'chapter' do
+    resources 'events', only: [:new, :create]
+  end
 
-  get 'events' => 'events#index', as: 'events'
-  get 'event/:id' => 'events#show', as: 'event'
-  get 'event/:id/edit' => 'events#edit', :as => 'edit_event'
-  get 'event/:id/manage' => 'events#manage', :as => 'manage_event'
-  patch 'event/:id' => 'events#update'
-  get 'chapter/:chapter_id/events/new' => 'events#new', as: 'new_event'
-  post 'chapter/:chapter_id/events' => 'events#create'
+  get 'events' => 'events#index'
+  resources 'events', only: [:edit, :show, :update], path: 'event'
 
-  get 'team/:id/edit' => 'teams#edit', as: 'edit_team'
-  patch 'team/:id' => 'teams#update', as: 'team'
-  post 'teams' => 'teams#create', as: 'teams'
-
-  get 'submission/:id/edit' => 'submissions#edit', as: 'edit_submission'
-  patch 'submission/:id' => 'submissions#update', as: 'submission'
-  post 'submissions' => 'submissions#create', as: 'submissions'
-
+  resources :teams, :submissions, only: [:edit, :update, :create]
 
   devise_for :admins, skip: :registrations
-  get 'admins/edit' => 'devise/registrations#edit', as: 'edit_admin_registration'
-  patch 'admins/:id' => 'devise/registrations#update', as: 'admin_registration'
+  devise_scope :admin do
+    get 'admins/edit' => 'devise/registrations#edit', as: 'edit_admin_registration'
+    patch 'admins/:id' => 'devise/registrations#update', as: 'admin_registration'
+  end
 end
